3bc8917a4f6dbf8db0a2112739fbc50c48e6f06d
WenerRodrigo <wener19.wr@gmail.com>
2023-12-06 11:04:59 -0300

    feat - validating card information , price , discount, buttons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import { api } from "../../services/api";
 import * as S from './styles';
 
 
+
 export const Home = () => {
     const [data, setData] = useState<any>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -31,23 +32,46 @@ export const Home = () => {
         return <S.Loanding>Carregando...</S.Loanding>
     }
 
+    const calculateDiscountPercentage = (originalPrice: number, salePrice: number) => {
+        if (originalPrice && salePrice) {
+            if (originalPrice === salePrice) {
+                return 0;
+            }
+
+            const discount = Math.min(1, salePrice / originalPrice) * 100;
+            return discount.toFixed(0); 
+        }
+        return 0;
+    }
+
     return (
         <>
             <S.Title>Conheça nossos Produtos</S.Title>
             <S.Content>
                 <S.SearchInput type="text"
-                    placeholder="Digite o nome do produto"
+                    placeholder="Buscar produtos, marcas e muito mais..."
                 />
-
                 <S.ContentCard>
                     {data.map((item: any) => (
                         <S.Card key={item.id}>
                             <S.ProductTitle>{item.title}</S.ProductTitle>
                             <S.ImageCard src={item.thumbnail} alt={item.title} />
                             <S.Price>R$: {item.price}</S.Price>
-                            <S.Button>+</S.Button>
-                            <span>0</span>
-                            <S.Button>-</S.Button>
+                            {item.original_price && (
+                                <>
+                                    <S.OriginalPrice>R$: {item.original_price}</S.OriginalPrice>
+                                    <S.SaleBadge>{calculateDiscountPercentage(item.original_price, item.price)}% OFF</S.SaleBadge>
+                                </>
+                            )}
+                            {item.installments && (
+                                <S.Installments>em 12x de R$: {item.installments.amount}</S.Installments>
+                            )}
+                            <S.ButtonContainer>
+                                <S.Button>+</S.Button>
+                                <span>0</span>
+                                <S.Button>-</S.Button>
+                            </S.ButtonContainer>
+                            {/* <S.ButtonComprar>Comprar</S.ButtonComprar> */}
                             <S.ButtonAddCart>Adicionar ao Carrinho</S.ButtonAddCart>
                         </S.Card>
                     ))}
