507bc53a6686f4035c4fe73aa003490d2f509891
WenerRodrigo <wener19.wr@gmail.com>
2023-12-06 13:58:45 -0300

    feat - created application context

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { CartProvider } from './components/context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <CartProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </CartProvider>
 );
